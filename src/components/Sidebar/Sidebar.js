fae367c9fa28ac341bc534d8ce4aa58112cc801f
Андрей Пожиленков <andrejpozilenkov@MacBook-Air-Andrej.local>
2023-08-13 16:55:52 +0300

    sidebar changes and sidebar elements added

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,15 +1,14 @@
 import React from "react";
 
 import classes from './Sidebar.module.css'
+import SidebarElement from "./SidebarElement";
 
 const Sidebar = (props) => {
 return (
 <div className={classes['sidebar']}>
-    <ul className={classes['nav-links']}>
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Contract</li>
+    <ul className={classes['nav-elements']}>
+        <li><SidebarElement>Home</SidebarElement></li>
+        <li><SidebarElement>About</SidebarElement></li>
     </ul>
 </div>
 );
---
+++ src/components/Sidebar/SidebarElement.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from './SidebarElement.module.css'
+
+const SidebarElement = (props) => {
+    return <div className={classes['sidebar-element']}>
+        {props.children}
+    </div>
+};
+
+export default SidebarElement;
