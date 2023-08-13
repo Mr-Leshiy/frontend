c3049bba286e351859acb2845763ef1859951089
Mr-Leshiy <leshiy12345678@gmail.com>
2023-08-13 20:30:35 +0300

    fix formating

--- src/components/Tickets/TicketCard.js
+++ src/components/Tickets/TicketCard.js
@@ -3,12 +3,18 @@ import React from "react";
 import classes from "./TicketCard.module.css";
 
 const TicketCard = (props) => {
-    return (
-        <div className={classes["ticket-card"]}>
-            <h5>{props.title}</h5>
-            <h5>{props.date.toLocaleDateString('en-us', {day: "numeric", year:"numeric", month:"short"})}</h5>
-        </div>
-    );
+  return (
+    <div className={classes["ticket-card"]}>
+      <h5>{props.title}</h5>
+      <h5>
+        {props.date.toLocaleDateString("en-us", {
+          day: "numeric",
+          year: "numeric",
+          month: "short",
+        })}
+      </h5>
+    </div>
+  );
 };
 
 export default TicketCard;
