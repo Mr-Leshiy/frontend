18a555d0cf418e38c4f3b39a9f287d6221459fc1
Mr-Leshiy <leshiy12345678@gmail.com>
2023-08-21 23:05:15 +0300

    add get_tickets endpoint

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -1,12 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from datetime import datetime
+import secrets
+
+from .types.models import TicketInfo
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/event/tickets/{account_id}")
+async def get_tickets(account_id: str):
+    tickets = []
+    for i in range(0, 30):
+        tickets.append(TicketInfo(secrets.token_hex(32), f"Ticket {i}", datetime(2023, 8, 31)))
+    return tickets
+
 
 def main():
     uvicorn.run("backend.app:app", host="0.0.0.0", port=8000, reload=True)
