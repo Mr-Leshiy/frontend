135351b3f93e03ae4e9e5e9e52f5ad9e6aa7aa04
Mr-Leshiy <leshiy12345678@gmail.com>
2023-08-23 20:27:12 +0300

    Make switching between pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
+import React, { useState } from "react";
+
 import Header from "./components/Header/Header.js";
 import Sidebar from "./components/Sidebar/Sidebar.js";
 import TicketsPage from "./components/TicketsPage/TicketsPage.js";
 import Row from "./components/UI/Row/Row.js";
 
+export const Pages = {
+  tickets: "tickets",
+  events: "events",
+};
+
 function App() {
+  const [activePage, setActivePage] = useState(Pages.tickets);
+
   return (
     <>
       <Header />
       <Row>
-        <Sidebar />
-        <TicketsPage />
+        <Sidebar setPage={setActivePage} currentPage={activePage} />
+        {activePage === Pages.tickets ? <TicketsPage /> : <></>}
       </Row>
     </>
   );
--- frontend/src/components/Sidebar/SidebarElement/SidebarElement.js
+++ frontend/src/components/Sidebar/SidebarElement/SidebarElement.js
@@ -3,7 +3,16 @@ import React from "react";
 import classes from "./SidebarElement.module.css";
 
 const SidebarElement = (props) => {
-  return <div className={classes["sidebar-element"]}>{props.text}</div>;
+  return (
+    <div
+      className={`${classes["sidebar-element"]} ${
+        props.isActive ? classes["sidebar-element-active"] : ""
+      }`}
+      onClick={props.onClick}
+    >
+      {props.text}
+    </div>
+  );
 };
 
 export default SidebarElement;
