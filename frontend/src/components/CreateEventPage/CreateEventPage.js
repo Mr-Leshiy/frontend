c59451cb9fcbad401bab5a3585ae00ea06b26a6e
Mr-Leshiy <leshiy12345678@gmail.com>
2023-08-23 21:44:30 +0300

    update

--- frontend/src/components/CreateEventPage/CreateEventPage.js
+++ frontend/src/components/CreateEventPage/CreateEventPage.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { useCardano } from "@cardano-foundation/cardano-connect-with-wallet";
 
 import classes from "./CreateEventPage.module.css";
 import PageTitle from "../UI/PageTitle/PageTitle";
 
 const CreateEventPage = () => {
-  return <div className={classes["create-event-page"]}>
-    <PageTitle title="Create Your Event" />
-  </div>;
+  const { isConnected } = useCardano();
+
+  const inlineStyles = {
+    pointerEvents: isConnected ? "auto" : "none",
+    opacity: isConnected ? "1" : "0.5",
+  };
+
+  return (
+    <div className={classes["create-event-page"]}>
+      <PageTitle title="Create Your Event" />
+      <div style={inlineStyles} className={classes["create-event-tab"]}></div>
+    </div>
+  );
 };
 
 export default CreateEventPage;
