e2fd44fc42dc261078bc5b60d8fa3ff8b74d509c
Mr-Leshiy <leshiy12345678@gmail.com>
2023-08-23 20:54:43 +0300

    add PageTitle component

--- frontend/src/components/CreateEventPage/CreateEventPage.js
+++ frontend/src/components/CreateEventPage/CreateEventPage.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 import classes from "./CreateEventPage.module.css";
+import PageTitle from "../UI/PageTitle/PageTitle";
 
 const CreateEventPage = () => {
-  return <></>;
+  return <div className={classes["create-event-page"]}>
+    <PageTitle title="Create Your Event" />
+  </div>;
 };
 
 export default CreateEventPage;
