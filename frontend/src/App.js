35dd65816e901591469b9473ecb015ce923af5bc
Mr-Leshiy <leshiy12345678@gmail.com>
2023-09-04 23:30:52 +0300

    add PageContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,20 @@ import Header from "./components/Header/Header.js";
 import Sidebar from "./components/Sidebar/Sidebar.js";
 import TicketsPage from "./components/TicketsPage/TicketsPage.js";
 import EventsPage from "./components/EventsPage/EventsPage.js";
+import PageContextProvider, {
+  Pages,
+  usePageContext,
+} from "./hooks/PageContext";
 
-export const Pages = {
-  tickets: "tickets",
-  events: "events",
+const PageHandler = () => {
+  const { activePage } = usePageContext();
+
+  return (
+    <>
+      {activePage === Pages.tickets ? <TicketsPage /> : null}
+      {activePage === Pages.events ? <EventsPage /> : null}
+    </>
+  );
 };
 
 function App() {
@@ -24,14 +34,15 @@ function App() {
   }, []);
 
   return (
-    <div className="app">
-      <Header header_ref={headerRef} />
-      <div ref={containerRef} className="container">
-        <Sidebar setPage={setActivePage} currentPage={activePage} />
-        {activePage === Pages.tickets ? <TicketsPage /> : <></>}
-        {activePage === Pages.events ? <EventsPage /> : <></>}
+    <PageContextProvider>
+      <div className="app">
+        <Header header_ref={headerRef} />
+        <div ref={containerRef} className="container">
+          <Sidebar />
+          <PageHandler />
+        </div>
       </div>
-    </div>
+    </PageContextProvider>
   );
 }
 
