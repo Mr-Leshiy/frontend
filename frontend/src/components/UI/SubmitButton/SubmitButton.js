8b9b5e2a5fc562b8d374f67343583b6ddcc0e362
Mr-Leshiy <leshiy12345678@gmail.com>
2023-09-06 19:41:02 +0300

    cleanup

--- frontend/src/components/UI/SubmitButton/SubmitButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import classes from "./SubmitButton.module.css";
-
-const SubmitButton = (props) => {
-  return <input type="submit" />;
-};
-
-export default SubmitButton;
