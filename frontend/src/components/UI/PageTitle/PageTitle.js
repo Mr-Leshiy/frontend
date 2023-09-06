8ae5847ada8492288bf979dd7e831e79ca5442db
Mr-Leshiy <leshiy12345678@gmail.com>
2023-09-06 19:32:05 +0300

    make PageTitle styling adaptive

--- frontend/src/components/UI/PageTitle/PageTitle.js
+++ frontend/src/components/UI/PageTitle/PageTitle.js
@@ -5,7 +5,7 @@ import classes from "./PageTitle.module.css";
 const PageTitle = (props) => {
   return (
     <div className={classes["title"]}>
-      <h1>{props.title}</h1>
+      {props.title}
     </div>
   );
 };
