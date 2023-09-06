4d2e992beab18d3ceea833aa477c7f70f6ad3f45
Mr-Leshiy <leshiy12345678@gmail.com>
2023-09-06 16:30:27 +0300

    add EventPage component

---
+++ frontend/src/components/EventPage/EventPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import classes from "./EventPage.module.css";
+import Page from "../UI/Page/Page";
+
+const EventPage = (props) => {
+  return <Page title={props.event.title}></Page>;
+};
+
+export default EventPage;
--- frontend/src/components/EventsPage/EventsList/EventsList.js
+++ frontend/src/components/EventsPage/EventsList/EventsList.js
@@ -4,8 +4,10 @@ import classes from "./EventsList.module.css";
 import { formatDate } from "../../../lib/Utils";
 import { useEventsContext } from "../../../hooks/EventsContext";
 import DeleteIcon from "../../../assets/delete.svg";
+import { Pages, usePageContext } from "../../../hooks/PageContext";
 
 const EventsList = (props) => {
+  const { setActivePage } = usePageContext();
   const { events, setEvents } = useEventsContext();
 
   const onRemoveHandler = (index) => {
@@ -22,8 +24,13 @@ const EventsList = (props) => {
     return true;
   });
 
+  const onChooseEvent = (event, index) => {
+    console.log(event);
+    setActivePage({ type: Pages.event, props: { event: event, index: index } });
+  };
+
   const eventRows = filteredEvents.map((event, i) => (
-    <tr key={i}>
+    <tr key={i} onClick={() => onChooseEvent(event, i)}>
       <td>{i + 1}</td>
       <td>{event.title}</td>
       <td>{formatDate(event.startDate) + " - " + formatDate(event.endDate)}</td>
