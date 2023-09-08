5a447f80fa007c7e23438e95fd91fbbca801f851
Mr-Leshiy <leshiy12345678@gmail.com>
2023-09-08 21:17:58 +0300

    add event page styling

--- frontend/src/components/EventPage/EventCard/EventCard.js
+++ frontend/src/components/EventPage/EventCard/EventCard.js
@@ -8,7 +8,7 @@ import UrlLogo from "../../../assets/url.svg";
 import EditLogo from "../../../assets/edit.svg";
 import { formatDate, formatTime } from "../../../lib/Utils";
 
-const EventCard = (props) => {
+const EventCard = ({ event }) => {
   return (
     <div className={classes["event-card"]}>
       <div className={classes["edit-button"]}>
@@ -19,12 +19,12 @@ const EventCard = (props) => {
         <h4>Start time</h4>
         <div className={classes["event-card-info-element"]}>
           <img src={CalendarLogo} alt="" />
-          <h3>{formatDate(new Date(props.event.startDate))}</h3>
+          <h3>{formatDate(new Date(event.startDate))}</h3>
         </div>
 
         <div className={classes["event-card-info-element"]}>
           <img src={ClockLogo} alt="" />
-          <h3>{formatTime(new Date(props.event.startDate))}</h3>
+          <h3>{formatTime(new Date(event.startDate))}</h3>
         </div>
       </div>
 
@@ -32,12 +32,12 @@ const EventCard = (props) => {
         <h4>End time</h4>
         <div className={classes["event-card-info-element"]}>
           <img src={CalendarLogo} alt="" />
-          <h3>{formatDate(new Date(props.event.endDate))}</h3>
+          <h3>{formatDate(new Date(event.endDate))}</h3>
         </div>
 
         <div className={classes["event-card-info-element"]}>
           <img src={ClockLogo} alt="" />
-          <h3>{formatTime(new Date(props.event.endDate))}</h3>
+          <h3>{formatTime(new Date(event.endDate))}</h3>
         </div>
       </div>
 
@@ -45,7 +45,7 @@ const EventCard = (props) => {
         <h4>Location</h4>
         <div className={classes["event-card-info-element"]}>
           <img src={LocationPinLogo} alt="" />
-          <h3>{props.event.location}</h3>
+          <h3>{event.location}</h3>
         </div>
       </div>
 
@@ -53,7 +53,7 @@ const EventCard = (props) => {
         <h4>Website</h4>
         <div className={classes["event-card-info-element"]}>
           <img src={UrlLogo} alt="" />
-          <h3>{props.event.website}</h3>
+          <h3>{event.website}</h3>
         </div>
       </div>
     </div>
--- frontend/src/components/EventPage/EventPage.js
+++ frontend/src/components/EventPage/EventPage.js
@@ -14,15 +14,31 @@ const EventPage = (props) => {
   };
 
   return (
-    <div>
+    <div className={classes["event-page"]}>
       <div className={classes["back-button"]} onClick={handleBackClick}>
         <img src={ArrowLeft} alt="" />
         <p>Back</p>
       </div>
 
       <Page title={props.event.title}>
-        <div className={classes["event-page"]}>
-          <EventCard event={props.event} />
+        <div className={classes["event-page-content"]}>
+          <div className={classes["event-info"]}>
+            <h4>Description</h4>
+            <div className={classes["event-info-description"]}>
+              Join us for an unforgettable evening of music, dance, and
+              entertainment. This event will showcase talented artists from
+              around the world, bringing together different cultures and styles.
+              Experience the energy and excitement as performers take the stage
+              and captivate the audience with their mesmerizing performances.
+              Don't miss out on this incredible event filled with creativity,
+              passion, and joy. Get your tickets now and be a part of this
+              magical celebration!
+            </div>
+          </div>
+
+          <div className={classes["event-card"]}>
+            <EventCard event={props.event} />
+          </div>
         </div>
       </Page>
     </div>
