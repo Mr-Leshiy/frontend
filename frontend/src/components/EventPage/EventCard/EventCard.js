8c2d7c736f6a5fc33c584fe2738795339d31a2b9
Mr-Leshiy <leshiy12345678@gmail.com>
2023-09-08 17:52:55 +0300

    add EventCard component

---
+++ frontend/src/components/EventPage/EventCard/EventCard.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+import classes from "./EventCard.module.css";
+import ClockLogo from "../../../assets/clock.svg";
+import CalendarLogo from "../../../assets/calendar.svg";
+import LocationPinLogo from "../../../assets/location-pin.svg";
+import UrlLogo from "../../../assets/url.svg";
+import { formatDate, formatTime } from "../../../lib/Utils";
+
+const EventCard = (props) => {
+  return (
+    <div className={classes["event-card"]}>
+
+      <div className={classes["event-card-info"]}>
+        <h4>Start time</h4>
+        <div className={classes["event-card-info-element"]}>
+          <img src={CalendarLogo} alt="" />
+          <h3>{formatDate(new Date(props.event.startDate))}</h3>
+        </div>
+
+        <div className={classes["event-card-info-element"]}>
+          <img src={ClockLogo} alt="" />
+          <h3>{formatTime(new Date(props.event.startDate))}</h3>
+        </div>
+      </div>
+
+      <div className={classes["event-card-info"]}>
+        <h4>End time</h4>
+        <div className={classes["event-card-info-element"]}>
+          <img src={CalendarLogo} alt="" />
+          <h3>{formatDate(new Date(props.event.endDate))}</h3>
+        </div>
+
+        <div className={classes["event-card-info-element"]}>
+          <img src={ClockLogo} alt="" />
+          <h3>{formatTime(new Date(props.event.endDate))}</h3>
+        </div>
+      </div>
+
+      <div className={classes["event-card-info"]}>
+        <h4>Location</h4>
+        <div className={classes["event-card-info-element"]}>
+          <img src={LocationPinLogo} alt="" />
+          <h3>{props.event.location}</h3>
+        </div>
+      </div>
+      
+      <div className={classes["event-card-info"]}>
+        <h4>Website</h4>
+        <div className={classes["event-card-info-element"]}>
+          <img src={UrlLogo} alt="" />
+          <h3>{props.event.website}</h3>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default EventCard;
--- frontend/src/components/EventPage/EventPage.js
+++ frontend/src/components/EventPage/EventPage.js
@@ -1,10 +1,29 @@
 import React from "react";
 
-// import classes from "./EventPage.module.css";
+import classes from "./EventPage.module.css";
 import Page from "../UI/Page/Page";
+import { usePageContext, Pages } from "../../hooks/PageContext";
+import ArrowLeft from "../../assets/arrow-left.svg";
+import EventCard from "./EventCard/EventCard";
 
 const EventPage = (props) => {
-  return <Page title={props.event.title}></Page>;
+  const { setActivePage } = usePageContext();
+
+  const handleBackClick = () => {
+    setActivePage({ type: Pages.events, props: {} });
+  };
+
+  return (
+    <div>
+      <div className={classes["back-button"]} onClick={handleBackClick}>
+        <img src={ArrowLeft} alt="" />
+        <p>Back</p>
+      </div>
+      <Page title={props.event.title}>
+        <EventCard event={props.event} />
+      </Page>
+    </div>
+  );
 };
 
 export default EventPage;
