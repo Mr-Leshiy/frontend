38f30499e2724a9bb69d6faa6e0673d17f9a2910
Mr-Leshiy <leshiy12345678@gmail.com>
2023-09-08 21:32:12 +0300

    update

--- frontend/src/components/EventPage/EventPage.js
+++ frontend/src/components/EventPage/EventPage.js
@@ -3,10 +3,11 @@ import React from "react";
 import classes from "./EventPage.module.css";
 import Page from "../UI/Page/Page";
 import { usePageContext, Pages } from "../../hooks/PageContext";
-import ArrowLeft from "../../assets/arrow-left.svg";
+import ArrowLeftLogo from "../../assets/arrow-left.svg";
+import EditLogo from "../../assets/edit.svg";
 import EventCard from "./EventCard/EventCard";
 
-const EventPage = (props) => {
+const EventPage = ({ event }) => {
   const { setActivePage } = usePageContext();
 
   const handleBackClick = () => {
@@ -14,31 +15,27 @@ const EventPage = (props) => {
   };
 
   return (
-    <div>
+    <div className={classes["container"]}>
       <div className={classes["back-button"]} onClick={handleBackClick}>
-        <img src={ArrowLeft} alt="" />
+        <img src={ArrowLeftLogo} alt="" />
         <p>Back</p>
       </div>
 
       <div className={classes["event-page"]}>
-        <Page title={props.event.title}>
+        <Page title={event.title}>
           <div className={classes["event-page-content"]}>
             <div className={classes["event-info"]}>
-              <h4>Description</h4>
+              <div className={classes["event-info-title"]}>
+                <h4>Description</h4>
+                <img src={EditLogo} alt="" />
+              </div>
               <div className={classes["event-info-description"]}>
-                Join us for an unforgettable evening of music, dance, and
-                entertainment. This event will showcase talented artists from
-                around the world, bringing together different cultures and
-                styles. Experience the energy and excitement as performers take
-                the stage and captivate the audience with their mesmerizing
-                performances. Don't miss out on this incredible event filled
-                with creativity, passion, and joy. Get your tickets now and be a
-                part of this magical celebration!
+                {event.description}
               </div>
             </div>
 
             <div className={classes["event-card"]}>
-              <EventCard event={props.event} />
+              <EventCard event={event} />
             </div>
           </div>
         </Page>
