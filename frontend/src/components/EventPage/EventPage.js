19cc1fef56ca0792b5f4d36d79b99eb5dd809000
Alex Pozhylenkov <leshiy12345678@gmail.com>
2023-09-09 11:02:33 +0300

    update EventPage styling (#32)

--- frontend/src/components/EventPage/EventPage.js
+++ frontend/src/components/EventPage/EventPage.js
@@ -1,10 +1,59 @@
 import React from "react";
 
-// import classes from "./EventPage.module.css";
-import Page from "../UI/Page/Page";
+import classes from "./EventPage.module.css";
 
-const EventPage = (props) => {
-  return <Page title={props.event.title}></Page>;
+import { usePageContext, Pages } from "../../hooks/PageContext";
+
+import ArrowLeftLogo from "../../assets/arrow-left.svg";
+import EditLogo from "../../assets/edit.svg";
+import ImageLogo from "../../assets/image.svg";
+
+import Page from "../UI/Page/Page";
+import EventCard from "./EventCard/EventCard";
+
+const EventPage = ({ event }) => {
+  const { setActivePage } = usePageContext();
+
+  const handleBackClick = () => {
+    setActivePage({ type: Pages.events, props: {} });
+  };
+
+  return (
+    <div className={classes["container"]}>
+      <div className={classes["tab"]}>
+        <div className={classes["back-button"]} onClick={handleBackClick}>
+          <img src={ArrowLeftLogo} alt="" />
+          <p>Back</p>
+        </div>
+      </div>
+
+      <div className={classes["event-page"]}>
+        <Page
+          title={<p className={classes["event-page-title"]}>{event.title}</p>}
+        >
+          <div className={classes["event-page-content"]}>
+            <div className={classes["event-info"]}>
+              <div className={classes["event-info-image"]}>
+                <img src={event.image ? event.image : ImageLogo} alt="" />
+              </div>
+
+              <div className={classes["event-info-title"]}>
+                <h4>Description</h4>
+                <img src={EditLogo} alt="" />
+              </div>
+              <div className={classes["event-info-description"]}>
+                {event.description}
+              </div>
+            </div>
+
+            <div className={classes["event-card"]}>
+              <EventCard event={event} />
+            </div>
+          </div>
+        </Page>
+      </div>
+    </div>
+  );
 };
 
 export default EventPage;
--- frontend/src/components/TicketsPage/TicketsList/TicketsList.js
+++ frontend/src/components/TicketsPage/TicketsList/TicketsList.js
@@ -1,23 +1,18 @@
 import React, { useState } from "react";
 
 import classes from "./TicketsList.module.css";
+import { filterEvents } from "../../UI/EventFilter/EventFilter";
 import TicketCard from "../TicketCard/TicketCard.js";
 import { getTickets } from "../../../lib/Tickets";
 
 const TicketsList = (props) => {
   const [tickets] = useState(getTickets());
 
-  const { value } = props.filterOptions;
-  const filteredTickets = tickets.filter((ticket) => {
-    if (value) {
-      const lower_case_value = value.toLowerCase();
-      return (
-        ticket.event.title.toLowerCase().includes(lower_case_value) ||
-        ticket.event.location.toLowerCase().includes(lower_case_value)
-      );
-    }
-    return true;
-  });
+  const filteredTickets = filterEvents(
+    tickets,
+    (ticket) => ticket.event,
+    props.filterOptions,
+  );
 
   const tickets_view = filteredTickets.map((ticket) => {
     return <TicketCard ticket={ticket} key={ticket.id} />;
