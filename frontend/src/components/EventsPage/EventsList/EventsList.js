c012d76c0b4fe8695794410710b9ccca8570c6c2
Alex Pozhylenkov <leshiy12345678@gmail.com>
2023-09-10 21:16:36 +0300

    Add sorting functionality (#36)

--- frontend/src/components/EventsPage/EventsList/EventsList.js
+++ frontend/src/components/EventsPage/EventsList/EventsList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./EventsList.module.css";
 
@@ -8,23 +8,58 @@ import { formatDate } from "../../../lib/Utils";
 
 import { filterEvents } from "../../UI/EventFilter/EventFilter";
 
-const EventsList = (props) => {
+const EventsList = ({ filterOptions }) => {
   const { setActivePage } = usePageContext();
   const { events } = useEventsContext();
-
   const filteredEvents = filterEvents(
-    events,
+    events.map((event, index) => ({ ...event, index })),
     (event) => event,
-    props.filterOptions,
+    filterOptions,
   );
+  const [sorting, setSorting] = useState({ column: null, direction: "asc" });
 
-  const onChooseEvent = (event, index) => {
-    setActivePage({ type: Pages.event, props: { eventIndex: index } });
+  const onChooseEvent = (event) => {
+    setActivePage({ type: Pages.event, props: { eventIndex: event.index } });
   };
 
-  const eventRows = filteredEvents.map((event, i) => (
-    <tr key={i} onClick={() => onChooseEvent(event, i)}>
-      <td>{i + 1}</td>
+  const sortByColumn = (column) => {
+    // Toggle sorting direction if the same column is clicked again
+    if (sorting.column === column) {
+      setSorting({
+        column,
+        direction: sorting.direction === "asc" ? "desc" : "asc",
+      });
+    } else {
+      setSorting({ column, direction: "asc" });
+    }
+  };
+
+  // Sort the events based on the selected column and direction
+  if (sorting.column === "index") {
+    filteredEvents.sort((a, b) => {
+      const direction = sorting.direction === "asc" ? 1 : -1;
+      return direction * (a.index - b.index);
+    });
+  } else if (sorting.column === "title") {
+    filteredEvents.sort((a, b) => {
+      const direction = sorting.direction === "asc" ? 1 : -1;
+      return direction * a.title.localeCompare(b.title);
+    });
+  } else if (sorting.column === "date") {
+    filteredEvents.sort((a, b) => {
+      const direction = sorting.direction === "asc" ? 1 : -1;
+      return direction * (a.startDate - b.startDate);
+    });
+  } else if (sorting.column === "location") {
+    filteredEvents.sort((a, b) => {
+      const direction = sorting.direction === "asc" ? 1 : -1;
+      return direction * a.location.localeCompare(b.location);
+    });
+  }
+
+  const eventRows = filteredEvents.map((event, _) => (
+    <tr key={event.index} onClick={() => onChooseEvent(event)}>
+      <td>{event.index + 1}</td>
       <td>{event.title}</td>
       <td>{formatDate(event.startDate) + " - " + formatDate(event.endDate)}</td>
       <td>{event.location}</td>
@@ -36,10 +71,26 @@ const EventsList = (props) => {
       <table>
         <thead>
           <tr>
-            <th>#</th>
-            <th>Title</th>
-            <th>Date</th>
-            <th>Location</th>
+            <th>
+              <div>
+                <p onClick={() => sortByColumn("index")}>#</p>
+              </div>
+            </th>
+            <th>
+              <div>
+                <p onClick={() => sortByColumn("title")}>Title</p>
+              </div>
+            </th>
+            <th>
+              <div>
+                <p onClick={() => sortByColumn("date")}>Date</p>
+              </div>
+            </th>
+            <th>
+              <div>
+                <p onClick={() => sortByColumn("location")}>Location</p>
+              </div>
+            </th>
           </tr>
         </thead>
         <tbody>{eventRows}</tbody>
